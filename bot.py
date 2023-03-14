2d2fe68b7f534434906bba25b8a6ac230a4883ee
Marvin Brückner <marvin@qdotnet.eu>
2023-03-14 17:43:44 +0100

    Removed not used code

--- bot.py
+++ bot.py
@@ -4,8 +4,10 @@ from discord.commands import Option
 import json
 
 intents = discord.Intents.default()
+intents.members = True
 bot = discord.Bot(
-    intents=intents
+    intents=intents,
+    members=True
 )
 
 
@@ -15,28 +17,33 @@ async def on_ready():
 
 
 @bot.event
-async def on_member_join(ctx, user: Option(discord.Member, default=None)):
-    category = discord.utils.get(ctx.guild.categories, name="Channel", category=category)
-    channel = await ctx.guild.create_text_channel(str(user.name) + "'s - Channel")
-    await channel.set_permissions(user, read_messages=True, send_messages=True, reason="User joined, Text Channel created")
-    await channel.set_permissions(user.guild.default_role, read_messages=False, send_messages=False, reason="User joined, Text Channel created")
+async def on_member_join(member):
+    category = discord.utils.get(member.guild.categories, name="Channel")
+    channel = await member.guild.create_text_channel(str(member.name), category=category)
+    if discord.utils.get(member.guild.channels, name=str(member.name)) is None:
+        await channel.set_permissions(member, read_messages=True, send_messages=True, reason="User joined, Text Channel created")
+        await channel.set_permissions(member.guild.default_role, read_messages=False, send_messages=False, reason="User joined, Text Channel created")
+    else:
+        log_channel = discord.utils.get(member.guild.channels, name="・logs・")
+        await log_channel.send(f"Der Textkanal von {member.name} wurde bereits erstellt")
+        return
+    
 
     embed = discord.Embed(
-        title=f"Textkanal von {user.name} wurde erfolgreich erstellt",
-        description=f"Der Textkanal von {user.name} wurde erfolgreich erstellt",
+        title=f"Textkanal von {member.name} wurde erfolgreich erstellt",
+        description=f"Der Textkanal von {member.name} wurde erfolgreich erstellt",
         color=discord.Color.gold()
     )
 
-    embed.set_thumbnail(url=user.display_avatar.url)
-    textchannel = discord.utils.get(ctx.guild.channels, name="・logs・")
+    embed.set_thumbnail(url=member.display_avatar.url)
+    textchannel = discord.utils.get(member.guild.channels, name="・logs・")
     await textchannel.send(embed=embed)
-    await ctx.respond(embed=embed, ephemeral=True)
 
 @bot.slash_command(description="Erstellt einen Textkanal")
 async def createchannel(ctx, user: Option(discord.Member, default=None)):
     category = discord.utils.get(ctx.guild.categories, name="Channel")
-    channel = await ctx.guild.create_text_channel(str(user.name) + "'s - Channel", category=category)
-    if discord.utils.get(ctx.guild.channels, name=str(user.name) + "'s - Channel") is None:
+    channel = await ctx.guild.create_text_channel(str(user.name), category=category)
+    if discord.utils.get(ctx.guild.channels, name=str(user.name)) is None:
         await channel.set_permissions(user, read_messages=True, send_messages=True, reason="User joined, Text Channel created")
         await channel.set_permissions(user.guild.default_role, read_messages=False, send_messages=False, reason="User joined, Text Channel created")
     else:
