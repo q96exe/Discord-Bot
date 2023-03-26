4fbe291b23052b281661f6c9e87d435c4b559930
Marvin Brückner <marvin@qdotnet.eu>
2023-03-26 01:47:00 +0100

    Update for Bleeter with resizing Images

--- bot.py
+++ bot.py
@@ -1,5 +1,9 @@
 import discord
 from discord.commands import Option
+from PIL import Image
+import requests
+import io
+import asyncio
 
 import aiosqlite
 
@@ -44,6 +48,18 @@ async def create_user(user_id, channel_id):
         )
         await db.commit()
 
+@bot.slash_command(name="removeuser", description="Entfernt einen User aus der Datenbank")
+async def remove_user_from_db(ctx, user: Option(discord.Member, required=True)):
+    async with aiosqlite.connect(DB) as db:
+        await db.execute(
+            """
+            DELETE FROM user WHERE user_id = ?
+            """,
+            (user.id,)
+        )
+        await db.commit()
+        await ctx.respond(f"Der User {user.name} wurde erfolgreich aus der Datenbank entfernt")
+
 async def check_user_exists(user_id):
     async with aiosqlite.connect(DB) as db:
         cursor = await db.execute(
@@ -66,22 +82,54 @@ async def get_channel_from_id(user_id):
         result = await cursor.fetchone()
         channel_id = result[0]
         return channel_id
+    
+async def resize_image_for_bleeter(attachments, channel):
+    for attachment in attachments:
+        url = attachment.url
+        response = requests.get(url)
+        image = Image.open(io.BytesIO(response.content))
+        aspect_ratio = image.width / image.height
+        new_width = int(650 * aspect_ratio)
+        resized_image = image.resize((new_width, 650))
         
+        output_buffer = await compress_image(resized_image)
         
-@bot.slash_command(description="DB Test", name="createdbuser")
-async def create_db_user(ctx, user: Option(discord.Member, default=None)):
-    await create_user(user.id, "1085244955657769071")
+        file = discord.File(output_buffer, filename="170123" + attachment.filename)
+        
+        embed = discord.Embed(
+            title="Bild erfolgreich angepasst",
+            description="Klicke auf den Button, um den Link des Bildes zu erhalten",
+            color=discord.Color.gold()
+        )
+    
+        attachment_message = await channel.send(file=file)
 
-    embed = discord.Embed(
-        title=f"User {user.name} wurde erfolgreich erstellt",
-        description=f"User {user.name} wurde erfolgreich erstellt",
-        color=discord.Color.gold()
-    )
+        attachment_url = attachment_message.attachments[0].url
+        button = discord.ui.Button(label="Link", style=discord.ButtonStyle.primary, url=attachment_url)
+        view = discord.ui.View()
+        view.add_item(button)
 
-    await ctx.respond(embed=embed, ephemeral=True)
+        await channel.send(embed=embed, view=view)
+
+
+async def compress_image(image, quality=100):
+    output_buffer = io.BytesIO()
+    image = image.convert("RGB")
+    image.save(output_buffer, format="JPEG", optimize=True, quality=quality)
+    while output_buffer.tell() > 1000000:
+        output_buffer.seek(0)
+        new_quality = int(quality * 0.9)
+        image.save(output_buffer, format="JPEG", optimize=True, quality=new_quality)
+        quality = new_quality
+        print(f"Image size: {output_buffer.tell()}")  # Überprüfen des Pufferinhalts
+    output_buffer.seek(0)
+    return output_buffer
+        
 
 @bot.event
 async def on_member_join(member):
+    role = discord.utils.get(member.guild.roles, name="User")
+    await member.add_roles(role)
     if await check_user_exists(member.id):
         existing_channel = bot.get_channel(int(await get_channel_from_id(member.id)))
         if existing_channel:
@@ -113,7 +161,10 @@ async def on_member_join(member):
 
         channel_embed = discord.Embed(
             title=f"Willkommen {member.name}!",
-            description=f"Dein eigener Textkanal wurde erfolgreich erstellt. Hier kannst du Bilder für z.B. Akten hochladen. \n\nSobald du ein Bild hochgeladen hast, wird dir ein Button angezeigt, mit dem du den korrekten Link des Bildes für die Akten erhalten kannst. \n\nWenn Fehler auftreten, kannst du diese gern bei {member.guild.owner.mention} melden.",
+            description=f"Dein eigener Textkanal wurde erfolgreich erstellt. Hier kannst du Bilder für z.B. Akten hochladen. \n\n\
+                Sobald du ein Bild hochgeladen hast, wird dir ein Button angezeigt, mit dem du den korrekten Link des Bildes für die Akten erhalten kannst. \n\n\
+                Außerdem kannst du das Bild direkt auf die passende Größe und Qualität für Bleeter anpassen. \n\n\
+                Wenn Fehler auftreten, kannst du diese gern bei {member.guild.owner.mention} melden.",
             color=discord.Color.gold()
         )
 
@@ -144,6 +195,8 @@ async def on_member_remove(member):
 async def on_message(message):
     if message.attachments:
         for attachment in message.attachments:
+            if(attachment.filename.startswith("170123")):
+                return
             if attachment.filename.endswith(".png") or attachment.filename.endswith(".jpg") or attachment.filename.endswith(".jpeg") or attachment.filename.endswith(".gif"):
                 link = attachment.url
                 if ".png" in link:
@@ -159,13 +212,16 @@ async def on_message(message):
                     index = link.index(".gif")
                     new_link = link[:index+len(".gif")] 
                 button = discord.ui.Button(label="Link", style=discord.ButtonStyle.primary, url=new_link)
+                bleeter_button = discord.ui.Button(label="Auf Bleetergröße anpassen", style=discord.ButtonStyle.primary)
+                bleeter_button.callback = lambda _: asyncio.create_task(resize_image_for_bleeter(message.attachments, message.channel))
                 view = discord.ui.View()
                 view.add_item(button)
+                view.add_item(bleeter_button)
 
                 embed = discord.Embed(
                     title="Download",
                     description="Klicke auf den Button, um den Link des Bildes zu erhalten",
-                    color=discord.Color.gold()
+                    color=discord.Color.brand_green()
                 )
                 
                 await message.channel.send(embed=embed, view=view)
